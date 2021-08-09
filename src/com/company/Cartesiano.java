4a96abf4a92f12dc167255715293eceeb1180387
Gustavo Leon <est.gustavo.leon@unimilitar.edu.co>
2021-08-09 09:47:55 -0500

    Signed-off-by: Gustavo Leon <est.gustavo.leon@unimilitar.edu.co>

---
+++ src/com/company/Cartesiano.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Cartesiano
+{
+    private float x;
+    private float y;
+
+    public Cartesiano(float x, float y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+
+    public Cartesiano polar_cartesiano(float radio, double angulo)
+    {
+        float x=radio*(float)Math.cos(angulo);
+        float y=radio*(float)Math.sin(angulo);
+        return new Cartesiano(x,y);
+    }
+
+    public Cartesiano polar_cartesiano1(Polar p)
+    {
+        return polar_cartesiano(p.getRadio(),p.getAngulo());
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+}
---
+++ src/com/company/Polar.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Polar
+{
+    private float radio;
+    private float angulo; //radianes
+
+    public Polar (float radio,float angulo)
+    {
+        this.radio=radio;
+        this.angulo=angulo;
+    }
+
+    public Polar cartesiano_polar(float x, float y)
+    {
+        float radio=(float)Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+        float angulo=(float)Math.atan(y/x);
+        return new Polar(x,y);
+    }
+
+    public Polar cartesiano_polar(Cartesiano c)
+    {
+        return cartesiano_polar(c.getX(),c.getY());
+    }
+
+    public float getRadio() {
+        return radio;
+    }
+
+    public void setRadio(float radio) {
+        this.radio = radio;
+    }
+
+    public float getAngulo() {
+        return angulo;
+    }
+
+    public void setAngulo(float angulo) {
+        this.angulo = angulo;
+    }
+}
